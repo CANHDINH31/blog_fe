0155831e990de366090f85ec0180daa9eb777ec8
dinhphamcanh <dinhpc@solashi.com>
2024-10-28 10:44:19 +0700

    fix: update text

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -7,38 +7,18 @@ const Hero = () => {
   return (
     <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
       <div className="mt-10 lg:w-1/2">
-        <h1 className="font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]">
+        <h1 className="text-center font-roboto text-3xl font-bold text-dark-soft md:text-5xl lg:max-w-[540px] lg:text-left lg:text-4xl xl:text-5xl">
           Chào mừng bạn đến với chúng tôi
         </h1>
-        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
+        <p className="mt-4 text-center text-dark-light md:text-xl lg:text-left lg:text-base xl:text-xl">
           Chúng tôi rất vui được giới thiệu những sản phẩm công nghệ tiên tiến
           nhất, giúp bạn nâng cao trải nghiệm sống và làm việc. Tại đây, bạn sẽ
           tìm thấy thông tin chi tiết, đánh giá và những trải nghiệm tuyệt vời
           từ các sản phẩm hàng đầu. Hãy cùng khám phá và tìm hiểu những công
           nghệ đột phá mà chúng tôi mang đến !
         </p>
-        <Search className="mt-10 lg:mt-6 xl:mt-10" />
-        <div className="flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7">
-          <span className="text-dark-light font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">
-            Chủ đề phổ biến:
-          </span>
-          <ul className="flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3 lg:text-sm xl:text-base">
-            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">
-              Website
-            </li>
-            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">
-              App
-            </li>
-            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">
-              Wordpress
-            </li>
-            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">
-              Đồ án
-            </li>
-          </ul>
-        </div>
       </div>
-      <div className="hidden lg:block lg:1/2">
+      <div className="lg:1/2 hidden lg:block">
         <img
           className="w-full"
           src={images.HeroImage}
